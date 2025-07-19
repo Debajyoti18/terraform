f8b950bb5c758a4e31ebe6a9d26f549263bfa157
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-07-19 06:44:29 +0000

    different files

---
+++ Day1/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value="ami-01637463b2cbe7cb6"
+instance_type_value = "t3.micro"
+subnet_id_value = "subnet-0125c6a3c582fe478"
---
+++ Day1/variable.tf
@@ -0,0 +1,10 @@
+
+variable "instance_type_value" {
+  description = "Instance  type "
+}
+variable "ami_value" {
+  description = "ami id of the instance"
+}
+variable "subnet_id_value" {
+  description = "What subnet instance would be lunch "
+}
