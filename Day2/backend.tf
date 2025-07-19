49883a3de5e27eec638a438c8c80beb939033153
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-07-19 16:34:57 +0000

    dynamodb

---
+++ Day2/backend.tf
@@ -0,0 +1,9 @@
+/*terraform {
+  backend "s3" {
+    bucket = "dj-123-b-2241002156"
+    key    = "debajyoti/terraform.tfstate"
+    region = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+*/
